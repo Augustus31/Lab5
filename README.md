778e39038c9823a7062406a80ae32f13ad872bd8
Augustus31 <supersumukh1@gmail.com>
2024-05-07 23:28:59 -0700

    tests

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Lab 5 
 
 Sumukh Murthy (no partner)
+
+Answers to Questions
+1. No. The message feature is a complex feature composed of many smaller functions/tasks and pieces of code. Unit tests are good for testing these individual tasks, not for complex features. If a test fails for the message feature, it will be difficult to find out why, since there is so much code inside.
+2. Yes. This is a small, self-contained task that is easy to test and easy to fix if it has a bug.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -9,3 +9,83 @@ import {
 } from '../code-to-unit-test/unit-test-me';
 
 // TODO - Part 2
+
+test('Is 5103459752 a phone number', () => {
+  expect(isPhoneNumber("(510)345-9752")).toBe(true);
+});
+
+test('Is 4723657854 a phone number', () => {
+  expect(isPhoneNumber("(472)365-7854")).toBe(true);
+});
+
+test('Is 34 a phone number', () => {
+  expect(isPhoneNumber("34")).toBe(false);
+});
+
+test('Is 5 a phone number', () => {
+  expect(isPhoneNumber("5")).toBe(false);
+});
+
+test('Is jobava@gmail.com a valid email', () => {
+  expect(isEmail("jobava@gmail.com")).toBe(true);
+});
+
+test('Is jalalabad@yahoo.com a valid email', () => {
+  expect(isEmail("jalalabad@yahoo.com")).toBe(true);
+});
+
+test('Is deusquiser a valid email', () => {
+  expect(isEmail("deusquiser")).toBe(false);
+});
+
+test('Is hadebrilhar a valid email', () => {
+  expect(isEmail("hadebrilhar")).toBe(false);
+});
+
+test('Is ordular_iN a strong pw', () => {
+  expect(isStrongPassword("ordular_iN")).toBe(true);
+});
+
+test('Is pekcokZaman a strong pw', () => {
+  expect(isStrongPassword("pekcokZaman")).toBe(true);
+});
+
+test('Is one a strong pw', () => {
+  expect(isStrongPassword("one")).toBe(false);
+});
+
+test('Is two a strong pw', () => {
+  expect(isStrongPassword("two")).toBe(false);
+});
+
+test('Is 10/04/2025 a date', () => {
+  expect(isDate("10/04/2025")).toBe(true);
+});
+
+test('Is 04/12/1204 a date', () => {
+  expect(isDate("04/12/1204")).toBe(true);
+});
+
+test('Is 04/1/12 a date', () => {
+  expect(isDate("04/1/12")).toBe(false);
+});
+
+test('Is 100/100/100 a date', () => {
+  expect(isDate("100/100/100")).toBe(false);
+});
+
+test('Is #6eeb34 a color', () => {
+  expect(isHexColor("#6eeb34")).toBe(true);
+});
+
+test('Is #6a6ade a color', () => {
+  expect(isHexColor("#6a6ade")).toBe(true);
+});
+
+test('Is 21 a color', () => {
+  expect(isHexColor("21")).toBe(false);
+});
+
+test('Is #sedeusquiser a color', () => {
+  expect(isHexColor("#sedeusquiser")).toBe(false);
+});
